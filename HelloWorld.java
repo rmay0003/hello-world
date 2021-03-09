8a9993061f69d3164e3a2ffbdbcb06bea41e9074
Richard May <rmay0003@email.cpcc.edu>
2021-03-09 16:41:34 -0500

    Revert "added program modified print line"
    This reverts commit e7d6e2f366cbe447e7376018869d65b611c64d5d.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld
    public static void main(String[] args)
    {
       System.out.printf("Hello world!");
-      System.out.println();
-      System.out.printf("Program Modified, but able to revert to original version using GitHub Desktop");
+      
    }
 }
